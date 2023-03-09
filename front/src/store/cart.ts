0f8d2dd960f5e9cd5d80af3e99b078d31441aad1
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-03-09 03:47:34 -0500

    feat: add cart page

--- front/src/store/cart.ts
+++ front/src/store/cart.ts
@@ -13,6 +13,10 @@ interface State {
 	length: number;
 }
 
+const saveInfoCart = (products: CartProduct) => {
+	localStorage.setItem(LocalData.ProductsInCart, JSON.stringify(products));
+};
+
 export const cartSlice = createSlice({
 	name: "cart",
 	initialState: {
@@ -29,10 +33,7 @@ export const cartSlice = createSlice({
 				};
 			}
 			state.length += 1;
-			localStorage.setItem(
-				LocalData.ProductsInCart,
-				JSON.stringify(state.products),
-			);
+			saveInfoCart(state.products);
 		},
 		setProducts(state: State, action: PayloadAction<CartProduct>) {
 			state.products = action.payload;
@@ -40,12 +41,42 @@ export const cartSlice = createSlice({
 				return acc + count;
 			}, 0);
 		},
+		removeOne(state: State, action: PayloadAction<Product["id"]>) {
+			if (state.products[action.payload]) {
+				const count = state.products[action.payload].count;
+				if (count > 0) {
+					state.products[action.payload].count -= 1;
+					state.length -= 1;
+				}
+				if (state.products[action.payload].count <= 0) {
+					state.products = Object.entries(state.products).reduce(
+						(acc, [k, v]) => {
+							if (action.payload === k) {
+								return acc;
+							}
+							return { ...acc, [k]: v };
+						},
+						{},
+					);
+				}
+				saveInfoCart(state.products);
+			}
+		},
+		addOne(state: State, action: PayloadAction<Product["id"]>) {
+			if (state.products[action.payload]) {
+				state.products[action.payload].count += 1;
+				state.length += 1;
+				saveInfoCart(state.products);
+			}
+		},
 	},
 });
 
 export const cartActions = {
 	addProductId: cartSlice.actions.addProductId,
 	setProducts: cartSlice.actions.setProducts,
+	removeOne: cartSlice.actions.removeOne,
+	addOne: cartSlice.actions.addOne,
 };
 
 export const cartReducer = cartSlice.reducer;
