a58c70d009a9f21088954b9b95eab58656d4878a
dreck2003 <dikson1.aranda.cabrera@gmail.com>
2023-03-09 11:37:46 -0500

    feat: add env variables to lead config data

--- front/src/utils/Product.ts
+++ front/src/utils/Product.ts
@@ -1,3 +1,4 @@
+import { Config } from "../config";
 import { Product } from "../interfaces";
 
 interface ProductFetch extends Product {
@@ -6,19 +7,19 @@ interface ProductFetch extends Product {
 
 export class ProductModel {
 	static async getProducts() {
-		const res = await fetch("http://localhost:5000/api/products");
+		const res = await fetch(`${Config.SERVER_URL}/api/products`);
 		const data = (await res.json()) as Array<ProductFetch>;
 		return data.map(({ _id, ...rest }) => {
-			return { ...rest, id: _id, image: `http://localhost:5000${rest.image}` };
+			return { ...rest, id: _id, image: `${Config.SERVER_URL}${rest.image}` };
 		});
 	}
 	static async getProduct(id: string) {
-		const res = await fetch(`http://localhost:5000/api/products/${id}`);
+		const res = await fetch(`${Config.SERVER_URL}/api/products/${id}`);
 		const data = (await res.json()) as ProductFetch;
 		return {
 			...data,
 			id: data._id,
-			image: `http://localhost:5000${data.image}`,
+			image: `${Config.SERVER_URL}${data.image}`,
 		};
 	}
 }
